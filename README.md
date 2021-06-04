acda0b5f3f407a4b65e1044dbfab8f9dc6cdf3c5
PyroPonya <43729142+PyroPonya@users.noreply.github.com>
2021-06-05 02:51:15 +0300

    1.0.0 - stat roller

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # DnD-dice-for-stats
  Basic stats roller for 5e ruleset of DnD
+	super simple to use 
+	2 btns -> 1st for rolls
+			  2nd for defalt array
+		+ temp save for last 6 rolls
---
+++ dice-roller/index.js
@@ -0,0 +1,58 @@
+const StandardArray5E = [15, 14, 13, 12, 10, 8];
+let currentRollFull = [0, 0, 0, 0, 0, 0];
+const rollHystory = [];
+
+const standardArray = () => {
+  currentRollFull = StandardArray5E;
+  setDiceValue();
+};
+
+const diceRoll = () => {
+  const currentRoll = [];
+  for (let i = 0; i < 4; i++) {
+    currentRoll.push(Math.floor(Math.random() * 6 + 1));
+  }
+  currentRoll
+    .sort(function (a, b) {
+      return b - a;
+    })
+    .pop();
+  return currentRoll.reduce((a, b) => a + b, 0);
+};
+
+const diceMultiRoll = () => {
+  currentRollFull = [];
+  for (let i = 0; i < 6; i++) {
+    currentRollFull[i] = " " + diceRoll();
+  }
+  currentRollFull.sort(function (a, b) {
+    return b - a;
+  });
+  if (rollHystory.length < 6) {
+    rollHystory.push(`${currentRollFull.slice(",").join(" - ")}`);
+  } else {
+    rollHystory.shift();
+    rollHystory.push(`${currentRollFull.slice(",").join(" - ")}`);
+  }
+  document.querySelector(".rollHistory").innerHTML = rollHystory
+    .map((el) => el + "</br>")
+    .slice(",")
+    .join(" ");
+  setDiceValue();
+};
+
+const setDiceValue = () => {
+  document.querySelector("#d1").innerHTML = currentRollFull[0] || 0;
+  document.querySelector("#d2").innerHTML = currentRollFull[1] || 0;
+  document.querySelector("#d3").innerHTML = currentRollFull[2] || 0;
+  document.querySelector("#d4").innerHTML = currentRollFull[3] || 0;
+  document.querySelector("#d5").innerHTML = currentRollFull[4] || 0;
+  document.querySelector("#d6").innerHTML = currentRollFull[5] || 0;
+};
+window.addEventListener("load", setDiceValue);
+
+document.querySelector(".rollBtn").addEventListener("click", diceMultiRoll);
+
+document
+  .querySelector(".rollStandardBtn")
+  .addEventListener("click", standardArray);
