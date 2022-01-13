85531d9fa74b1955853a62f82d917d35a91ceb69
PyroPonya <43729142+PyroPonya@users.noreply.github.com>
2022-01-13 14:19:10 +0300

    ux features

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const StandardArray5E = [15, 14, 13, 12, 10, 8];
 let currentRollFull = [0, 0, 0, 0, 0, 0];
 const rollHystory = [];
+const rollBtn = document.querySelector('.rollBtn');
+const rollStandardBtn = document.querySelector('.rollStandardBtn');
 
 const standardArray = () => {
-  currentRollFull = StandardArray5E;
-  setDiceValue();
+  setDiceValue(StandardArray5E);
 };
 
 const diceRoll = () => {
@@ -21,45 +22,58 @@ const diceRoll = () => {
 };
 
 const diceMultiRoll = () => {
-  currentRollFull = [];
-  for (let i = 0; i < 6; i++) {
-    currentRollFull[i] = " " + diceRoll();
-  }
-  currentRollFull.sort(function (a, b) {
-    return b - a;
-  });
+  let arrWithMaxNum = [0];
+  for (let sixRolls = 0; sixRolls < 6; sixRolls++) {
+    currentRollFull = [];
+    for (let i = 0; i < 6; i++) {
+      currentRollFull[i] = ' ' + diceRoll();
+    }
+    currentRollFull.sort(function (a, b) {
+      return b - a;
+    });
 
-  for (let i = 0; i < 6; i++) {
-    if (currentRollFull[i] < 10) {
-      currentRollFull[i] = ("0" + currentRollFull[i]).replace(/\s/g, "");
+    for (let i = 0; i < 6; i++) {
+      if (currentRollFull[i] < 10) {
+        currentRollFull[i] = ('0' + currentRollFull[i]).replace(/\s/g, '');
+      }
+    }
+
+    if (rollHystory.length < 6) {
+      rollHystory.push(`${currentRollFull.slice(',').join(' - ')}`);
+    } else {
+      rollHystory.shift();
+      rollHystory.push(`${currentRollFull.slice(',').join(' - ')}`);
+    }
+    document.querySelector('.rollHistory').innerHTML = rollHystory
+      .map((el) => el + '</br>')
+      .slice(',')
+      .join(' ');
+    if (currentRollFull[0] > arrWithMaxNum[0]) {
+      arrWithMaxNum = currentRollFull.map((el) => el);
+    } else if (
+      currentRollFull[0] == arrWithMaxNum[0] &&
+      currentRollFull[1] > arrWithMaxNum[1]
+    ) {
+      arrWithMaxNum = currentRollFull.map((el) => el);
     }
   }
-
-  if (rollHystory.length < 6) {
-    rollHystory.push(`${currentRollFull.slice(",").join(" - ")}`);
-  } else {
-    rollHystory.shift();
-    rollHystory.push(`${currentRollFull.slice(",").join(" - ")}`);
-  }
-  document.querySelector(".rollHistory").innerHTML = rollHystory
-    .map((el) => el + "</br>")
-    .slice(",")
-    .join(" ");
-  setDiceValue();
+  setDiceValue(arrWithMaxNum);
+  /**=============== Situational rules ===============**/
+  // rollBtn.removeEventListener("click", diceMultiRoll);
+  // rollBtn.style.display = "none";
+  // rollStandardBtn.style.display = "none";
 };
 
-const setDiceValue = () => {
-  document.querySelector("#d1").innerHTML = currentRollFull[0] || 0;
-  document.querySelector("#d2").innerHTML = currentRollFull[1] || 0;
-  document.querySelector("#d3").innerHTML = currentRollFull[2] || 0;
-  document.querySelector("#d4").innerHTML = currentRollFull[3] || 0;
-  document.querySelector("#d5").innerHTML = currentRollFull[4] || 0;
-  document.querySelector("#d6").innerHTML = currentRollFull[5] || 0;
+const setDiceValue = (arr) => {
+  document.querySelector('#d1').textContent = arr[0] || 0;
+  document.querySelector('#d2').textContent = arr[1] || 0;
+  document.querySelector('#d3').textContent = arr[2] || 0;
+  document.querySelector('#d4').textContent = arr[3] || 0;
+  document.querySelector('#d5').textContent = arr[4] || 0;
+  document.querySelector('#d6').textContent = arr[5] || 0;
 };
-window.addEventListener("load", setDiceValue);
+window.addEventListener('load', setDiceValue);
 
-document.querySelector(".rollBtn").addEventListener("click", diceMultiRoll);
+rollBtn.addEventListener('click', diceMultiRoll);
 
-document
-  .querySelector(".rollStandardBtn")
-  .addEventListener("click", standardArray);
+rollStandardBtn.addEventListener('click', standardArray);
